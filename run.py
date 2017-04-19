d9c6e7cfa3d9f18de7bfbb9a62014d6338692df4
arunjohnkuruvilla <arunjohnkuruvilla@gmail.com>
2017-04-19 17:58:18 -0400

    test

--- run.py
+++ run.py
@@ -63,7 +63,7 @@ def bootstrap():
     	# Get current working directory
     	current_path = str(os.path.dirname(os.path.realpath(__file__)))
 
-    	argument_line = current_path + '\\' + sys.argv[0]
+    	argument_line = "python " + current_path + '\\' + sys.argv[0]
     	print argument_line
         hinstance = ctypes.windll.shell32.ShellExecuteW(
             None, 
