ba0ceac96c4256c4271b05725073c66a30dd73b5
arunjohnkuruvilla <arunjohnkuruvilla@gmail.com>
2017-04-19 18:02:13 -0400

    test

--- run.py
+++ run.py
@@ -23,68 +23,68 @@ import os
 
 class SW(enum.IntEnum):
 
-    HIDE = 0
-    MAXIMIZE = 3
-    MINIMIZE = 6
-    RESTORE = 9
-    SHOW = 5
-    SHOWDEFAULT = 10
-    SHOWMAXIMIZED = 3
-    SHOWMINIMIZED = 2
-    SHOWMINNOACTIVE = 7
-    SHOWNA = 8
-    SHOWNOACTIVATE = 4
-    SHOWNORMAL = 1
+	HIDE = 0
+	MAXIMIZE = 3
+	MINIMIZE = 6
+	RESTORE = 9
+	SHOW = 5
+	SHOWDEFAULT = 10
+	SHOWMAXIMIZED = 3
+	SHOWMINIMIZED = 2
+	SHOWMINNOACTIVE = 7
+	SHOWNA = 8
+	SHOWNOACTIVATE = 4
+	SHOWNORMAL = 1
 
 
 class ERROR(enum.IntEnum):
 
-    ZERO = 0
-    FILE_NOT_FOUND = 2
-    PATH_NOT_FOUND = 3
-    BAD_FORMAT = 11
-    ACCESS_DENIED = 5
-    ASSOC_INCOMPLETE = 27
-    DDE_BUSY = 30
-    DDE_FAIL = 29
-    DDE_TIMEOUT = 28
-    DLL_NOT_FOUND = 32
-    NO_ASSOC = 31
-    OOM = 8
-    SHARE = 26
+	ZERO = 0
+	FILE_NOT_FOUND = 2
+	PATH_NOT_FOUND = 3
+	BAD_FORMAT = 11
+	ACCESS_DENIED = 5
+	ASSOC_INCOMPLETE = 27
+	DDE_BUSY = 30
+	DDE_FAIL = 29
+	DDE_TIMEOUT = 28
+	DLL_NOT_FOUND = 32
+	NO_ASSOC = 31
+	OOM = 8
+	SHARE = 26
 
 
 def bootstrap():
-    if ctypes.windll.shell32.IsUserAnAdmin():
-        main()
-    else:
-    	# ret = shell32.ShellExecuteW(None, u"runas", executable, "/k " + argument_line, None, 1)
+	if ctypes.windll.shell32.IsUserAnAdmin():
+		main()
+	else:
+		# ret = shell32.ShellExecuteW(None, u"runas", executable, "/k " + argument_line, None, 1)
 
-    	# Get current working directory
-    	current_path = str(os.path.dirname(os.path.realpath(__file__)))
+		# Get current working directory
+		current_path = str(os.path.dirname(os.path.realpath(__file__)))
 
-    	argument_line = "python " + current_path + '\\' + sys.argv[0]
-    	print argument_line
-    	try:
-	        hinstance = ctypes.windll.shell32.ShellExecuteW(
-	            None, 
-	            'runas', 
-	            sys.executable, 
-	            argument_line, 
-	            None, 
-	            SW.SHOWNORMAL
-	        )
-	    except Exception as e:
-	    	print e.message
-	    	
-        if hinstance <= 32:
-            raise RuntimeError(ERROR(hinstance))
+		argument_line = "python " + current_path + '\\' + sys.argv[0]
+		print argument_line
+		try:
+			hinstance = ctypes.windll.shell32.ShellExecuteW(
+				None, 
+				'runas', 
+				sys.executable, 
+				argument_line, 
+				None, 
+				SW.SHOWNORMAL
+			)
+		except Exception as e:
+			print e.message
+			
+		if hinstance <= 32:
+			raise RuntimeError(ERROR(hinstance))
 
 
 def main():
 	print "hi"
-    # Your Code Here
+	# Your Code Here
 
 
 if __name__ == '__main__':
-    bootstrap()
+	bootstrap()
