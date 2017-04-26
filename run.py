1e66e9ef4fea7622f3ef1dd0f6f699bf6c1ad846
arunjohnkuruvilla <arunjohnkuruvilla@gmail.com>
2017-04-25 20:22:09 -0400

    test

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ def main():
 		monitor_object = monitor.Monitor()
 		monitor_object.initialize()
 	else:
-		admin.bootstrap(sys.argv[0])
+		bootstrap(sys.argv[0])
 		sys.exit(0)
 
 if __name__ == '__main__':
